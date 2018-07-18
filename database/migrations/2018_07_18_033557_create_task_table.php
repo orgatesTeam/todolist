c9ac6b9f5f50d6a20d8a82d4d8139274ea952920
chen <abc49906132@gmail.com>
2018-07-18 11:57:02 +0800

    create schema

--- database/migrations/2018_07_18_033557_create_task_table.php
+++ database/migrations/2018_07_18_033557_create_task_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTaskTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('task', function (Blueprint $table) {
+            $table->increments('T_id');
+            $table->foreign('U_id')->references('id')->on('users');
+            $table->string('task_name');
+            $table->string('content');
+            $table->date('time');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('task');
     }
 }
